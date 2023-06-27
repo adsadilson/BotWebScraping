b118b0b5606215d604ce8eafedd3940a2fe0de60
adsadilson <adilson.curso@yahoo.com.br>
2023-06-27 13:43:54 -0300

    start do projetco

---
+++ src/main/java/br/com/apssystem/brasileiraoapi/emuns/StatusPartida.java
@@ -0,0 +1,11 @@
+package br.com.apssystem.brasileiraoapi.emuns;
+
+import lombok.Getter;
+
+@Getter
+public enum StatusPartida {
+    PARTIDA_INICIADA,
+    PARTIDA_EM_ANDAMENTO,
+    PARTIDA_FINALIZADA,
+    PARTIDA_PENATIS;
+}
